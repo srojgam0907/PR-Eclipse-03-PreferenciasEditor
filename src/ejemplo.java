c498e88d539e64dc5b4cb4e39a874fb06831fdfd
Sara rojas <srojgam0907@g.educaand.es>
2025-10-18 11:27:57 +0200

    Create ejemplo.java

---
+++ src/ejemplo.java
@@ -0,0 +1,8 @@
+public class Ejemplo {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x < 10) {
+            System.out.println("Valor menor que 10");
+        }
+    }
+}
