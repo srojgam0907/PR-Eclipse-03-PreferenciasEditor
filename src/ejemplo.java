4cc6f609408953f10b15d511120df4373a2628c7
Sara rojas <srojgam0907@g.educaand.es>
2025-10-18 12:49:22 +0200

    Update ejemplo.java

--- src/ejemplo.java
+++ src/ejemplo.java
@@ -1,8 +1,21 @@
-public class Ejemplo {
-    public static void main(String[] args) {
-        int x = 5;
-        if (x < 10) {
-            System.out.println("Valor menor que 10");
-        }
-    }
+package practica_5;
+import java.util.Scanner;
+
+public class ejemplo {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner keyboard = new Scanner(System.in);
+		int x; 
+
+		System.out.println("Introduce un numero positivo menor que 10");
+		x = keyboard.nextInt();
+
+		if (x >= 0 && x < 10) {
+			System.out.println(x + " es menor que 10");
+
+		} else {
+			System.out.println("ERROR. El numero tiene que ser positivo y menor que 10");
+			
+		}
+	}
 }
